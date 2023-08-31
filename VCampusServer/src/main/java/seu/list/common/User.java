69ee8e99800fdf19d8eabfa53b56e8d5a21211c0
XI12360 <128698003+xi12360@users.noreply.github.com>
2023-08-31 19:58:12 +0800

    Update User.java

--- VCampusServer/src/main/java/seu/list/common/User.java
+++ VCampusServer/src/main/java/seu/list/common/User.java
@@ -4,6 +4,8 @@ package seu.list.common;
 import seu.list.server.dao.CourseDaoImp;
 import seu.list.server.db.SqlHelperImp;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
@@ -23,7 +25,7 @@ public class User implements java.io.Serializable {
     private String pwd;//6
     private String role;//7
     private String money;//8
-
+    private String online;//9
 
     public void print() {
         System.out.print(id + "\n" + name + "\n" + age + "\n" + sex + "\n" + major + "\n" + grade + "\n" + pwd + "\n" + role + "\n" + money + "\n" + "\n");
@@ -42,6 +44,7 @@ public class User implements java.io.Serializable {
         pwd = content.get(6);
         role = content.get(7);
         money = content.get(8);
+        online=content.get(9);
     }
 
     ;
@@ -168,6 +171,11 @@ public class User implements java.io.Serializable {
     public void setMoney(String money) {
         this.money = money;
     }
+    /**
+     * @return 在线状态
+     */
+    public boolean getState(){return online == "1";}
+
 
     /**
      * @return 用户信息
@@ -183,9 +191,24 @@ public class User implements java.io.Serializable {
         userContents.add(pwd);
         userContents.add(role);
         userContents.add(money);
+        userContents.add(online);
         return userContents;
     }
 
+    public void changeState(int online){
+        String []paras=new String[1];
+        paras[0]=this.id;
+        if(online==1){
+            this.online="1";
+            String sql="update tb_User set uOnline='" + "1" + "'where uID= ?";
+            new SqlHelperImp().sqlUpdate(sql, paras);
+        }
+        else {
+            this.online="0";
+            String sql="update tb_User set uOnline='" + "0" + "'where uID= ?";
+            new SqlHelperImp().sqlUpdate(sql, paras);
+        }
+    }
     /**
      * @return 用户已选课程
      */
@@ -212,6 +235,7 @@ public class User implements java.io.Serializable {
                 ", pwd='" + getPwd() + "'" +
                 ", role='" + getRole() + "'" +
                 ", money='" + getMoney() + "'" +
+                ", online='" + getState() + "'"+
                 "}";
     }
 }

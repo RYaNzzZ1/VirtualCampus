26ceee5dac5fe6c6e6fc6192a308c6f0fc7c667c
XI12360 <128698003+xi12360@users.noreply.github.com>
2023-09-06 21:53:43 +0800

    Update ChatServer.java

--- VCampusServer/src/main/java/seu/list/server/dao/ChatServer.java
+++ VCampusServer/src/main/java/seu/list/server/dao/ChatServer.java
@@ -29,11 +29,40 @@ public class ChatServer extends Chat_DbAccess {
         con = getConnection();
         mesToClient = new Message();
         switch (this.mesFromClient.getMessageType()) {
+            case MessageType.CRE_ANONYMOUS:{
+                Random random=new Random();
+                // 随机生成一个姓名，长度为 2 到 6 个字母
+                int nameLength = random.nextInt(5) + 2;
+                String ano;
+                StringBuilder name ;
+                //匿名不重复
+                do {
+                    name =new StringBuilder();
+                    for (int j = 0; j < nameLength; j++) {
+                        char letter = ALPHABET.charAt(random.nextInt(ALPHABET.length()));
+                        name.append(letter);
+                    }
+                    ano=name.toString();
+                }while(ServerClientThreadMgr.contain(ano));
+
+                ServerClientThreadMgr.bindano(uid,ano);
+                mesToClient.setData(ano);
+                break;
+            }
+            case MessageType.DES_ANONYMOUS:{
+                //退出登录时清除匿名
+                ServerClientThreadMgr.unbindano(uid);
+                break;
+            }
             case MessageType.ChatShow:
                 break;
-            case MessageType.ChatSend:
+            case MessageType.ChatSend: {
                 Chat chat = (Chat) this.mesFromClient.getData();
-                this.mesToClient.setData(chatBroadcast(chat));
+                this.sendChat(chat);
+                this.mesToClient.setData(this.chatBroadcast(chat));
+                break;
+            }
+        }
             case MessageType.ChatHistory: {
                 System.out.println("serving CHAT_HISTORY");
                 System.out.println("grabbing.....");
@@ -55,12 +84,16 @@ public class ChatServer extends Chat_DbAccess {
             }
         }
     }
+        private void sendChat(Chat chat) {//匿名，内容，时间，uID
+        String sql="insert into tb_Chat(NickName,ChatText,ChatTime,uID) values(?,?,?,?)";
+        String[] paras = new String[4];
+        paras[0]=chat.getNickName();
+        paras[1]=chat.getChatText();
+        paras[2]=chat.getChatTime();
+        paras[3]=chat.getUID();
 
-
-
-
-
-
+        new SqlHelper().sqlUpdate(sql,paras);
+    }
     private Message[] chatBroadcast(Chat chat) throws SQLException {
         Map<String, ServerSocketThread> threadPool = ServerClientThreadMgr.getPool();
         ArrayList<String> onlineID = getChatOnline(chat.getUID());

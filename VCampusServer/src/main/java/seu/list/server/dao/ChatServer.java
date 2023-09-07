5517b6c4f9ef1c0b96c339bea670b61e86eb915a
RyanZhao <437071476@qq.com>
2023-09-07 10:33:03 +0800

    完成了chatshow chat 后端基本完成

--- VCampusServer/src/main/java/seu/list/server/dao/ChatServer.java
+++ VCampusServer/src/main/java/seu/list/server/dao/ChatServer.java
@@ -1,15 +1,20 @@
 package seu.list.server.dao;
 
-import com.sun.security.ntlm.NTLMException;
+
 import seu.list.client.driver.Client;
-import seu.list.common.*;
+import seu.list.common.Chat;
+import seu.list.common.Message;
+import seu.list.common.MessageType;
+import seu.list.common.ModuleType;
 import seu.list.server.db.Chat_DbAccess;
 import seu.list.server.db.SqlHelper;
 import seu.list.server.driver.ServerClientThreadMgr;
 import seu.list.server.driver.ServerSocketThread;
 
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 public class ChatServer extends Chat_DbAccess {
@@ -20,50 +25,52 @@ public class ChatServer extends Chat_DbAccess {
     private Message mesFromClient; // 从客户端收到的数据
     private Message mesToClient;
 
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//字母表
+
     public ChatServer(Message mesFromClient) {
         super();
         this.mesFromClient = mesFromClient;
     }
 
-    public void execute() throws SQLException, NTLMException {
+    public void execute() throws SQLException {
         con = getConnection();
         mesToClient = new Message();
+        String uid = mesFromClient.getUserId();
         switch (this.mesFromClient.getMessageType()) {
-            case MessageType.CRE_ANONYMOUS:{
-                Random random=new Random();
+            case MessageType.CRE_ANONYMOUS:
+                Random random = new Random();
                 // 随机生成一个姓名，长度为 2 到 6 个字母
                 int nameLength = random.nextInt(5) + 2;
                 String ano;
-                StringBuilder name ;
+                StringBuilder name;
                 //匿名不重复
                 do {
-                    name =new StringBuilder();
+                    name = new StringBuilder();
                     for (int j = 0; j < nameLength; j++) {
                         char letter = ALPHABET.charAt(random.nextInt(ALPHABET.length()));
                         name.append(letter);
                     }
-                    ano=name.toString();
-                }while(ServerClientThreadMgr.contain(ano));
+                    ano = name.toString();
+                } while (ServerClientThreadMgr.contain(ano));
 
-                ServerClientThreadMgr.bindano(uid,ano);
+                ServerClientThreadMgr.bindano(uid, ano);
                 mesToClient.setData(ano);
                 break;
-            }
-            case MessageType.DES_ANONYMOUS:{
+            case MessageType.DES_ANONYMOUS: {
                 //退出登录时清除匿名
                 ServerClientThreadMgr.unbindano(uid);
                 break;
             }
             case MessageType.ChatShow:
+                ArrayList<Chat> allChats = this.getAllChat();
+                this.mesToClient.setData(allChats);
                 break;
-            case MessageType.ChatSend: {
+            case MessageType.ChatSend:
                 Chat chat = (Chat) this.mesFromClient.getData();
                 this.sendChat(chat);
                 this.mesToClient.setData(this.chatBroadcast(chat));
                 break;
-            }
-        }
-            case MessageType.ChatHistory: {
+            case MessageType.ChatHistory:
                 System.out.println("serving CHAT_HISTORY");
                 System.out.println("grabbing.....");
                 Vector<String> sigChatContent = new Vector<String>();
@@ -81,30 +88,30 @@ public class ChatServer extends Chat_DbAccess {
                 this.mesToClient.setContent(allChatContent);
                 System.out.println("CHAT_HISTORY finished");
                 break;
-            }
         }
     }
-        private void sendChat(Chat chat) {//匿名，内容，时间，uID
-        String sql="insert into tb_Chat(NickName,ChatText,ChatTime,uID) values(?,?,?,?)";
-        String[] paras = new String[4];
-        paras[0]=chat.getNickName();
-        paras[1]=chat.getChatText();
-        paras[2]=chat.getChatTime();
-        paras[3]=chat.getUID();
 
-        new SqlHelper().sqlUpdate(sql,paras);
+    private void sendChat(Chat chat) {//匿名，内容，时间，uID
+        String sql = "insert into tb_Chat(NickName,ChatText,ChatTime,uID) values(?,?,?,?)";
+        String[] paras = new String[4];
+        paras[0] = chat.getNickName();
+        paras[1] = chat.getChatText();
+        paras[2] = chat.getChatTime();
+        paras[3] = chat.getUID();
+        new SqlHelper().sqlUpdate(sql, paras);
     }
+
     private Message[] chatBroadcast(Chat chat) throws SQLException {
         Map<String, ServerSocketThread> threadPool = ServerClientThreadMgr.getPool();
         ArrayList<String> onlineID = getChatOnline(chat.getUID());
-        Message[] messageResponse=new Message[onlineID.size()];
+        Message[] messageResponse = new Message[onlineID.size()];
         for (int i = 0; i < onlineID.size(); i++) {
             Message messageBroadcast = new Message();
             messageBroadcast.setModuleType(ModuleType.Chat);
             messageBroadcast.setMessageType(MessageType.ChatShow);
             messageBroadcast.setData(chat);
-            Client client=new Client(threadPool.get(onlineID.get(i)).getClientSocket());
-            messageResponse[i]=client.sendRequestToServer(messageBroadcast);
+            Client client = new Client(threadPool.get(onlineID.get(i)).getClientSocket());
+            messageResponse[i] = client.sendRequestToServer(messageBroadcast);
         }
         return messageResponse;
     }
@@ -119,10 +126,8 @@ public class ChatServer extends Chat_DbAccess {
         return uID;
     }
 
-    public List<Chat> getAllChat()
-    {
+    public ArrayList<Chat> getAllChat() {
         String sql = "select * from tb_Chat";
-        return new SqlHelper().sqlChatQuery(sql, new String[]{});
+        return (ArrayList<Chat>) new SqlHelper().sqlChatQuery(sql, new String[]{});
     }
-
 }

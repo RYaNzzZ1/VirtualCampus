818e072f774e4201523a2136d256e6bb0b1a1526
XI12360 <128698003+xi12360@users.noreply.github.com>
2023-09-08 08:41:07 +0800

    Update ClientLoginFrame.java

--- VCampusClient/src/main/java/seu/list/client/view/ClientLoginFrame.java
+++ VCampusClient/src/main/java/seu/list/client/view/ClientLoginFrame.java
@@ -151,8 +151,9 @@ public class ClientLoginFrame extends JFrame implements ActionListener {
                 Message res = ccs.sendRequestToServer(mes);
                 int sign = res.getUserType();
                 u = (User) res.getData();
-
-                if (sign == 0 || sign == 1 || sign == 30 || sign == 31) {
+                if(u == null)
+                    JOptionPane.showMessageDialog(null, "用户名或密码错误！", "错误", JOptionPane.ERROR_MESSAGE);
+                else if (sign == 0 || sign == 1 || sign == 30 || sign == 31) {
                     if (sign >= 30) {//重复登录，之前的下线
                         sign -= 30;
                         JOptionPane.showMessageDialog(null, "重复登录，之前的连接已断开", "提示", JOptionPane.WARNING_MESSAGE);
@@ -160,9 +161,7 @@ public class ClientLoginFrame extends JFrame implements ActionListener {
                     this.setVisible(false);
                     MainMenu csf = new MainMenu(sign, u.getId(), u.getPwd(), u.getName(), u.getMoney(), this.socket);
                     csf.setVisible(true);
-                } else if (sign == 2) {
-                    JOptionPane.showMessageDialog(null, "用户名或密码错误！", "错误", JOptionPane.ERROR_MESSAGE);
-                } else {
+                } else if(sign == 2){
                     JOptionPane.showMessageDialog(null, "用户名或密码错误！", "错误", JOptionPane.ERROR_MESSAGE);
                 }
 
